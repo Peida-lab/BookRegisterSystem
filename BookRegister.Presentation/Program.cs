d58dc7c83777c16996881b7b966e13474aa80213
Ali Peida <din GitHub-email>
2026-09-12 01:21:35 +0200

    lyser fortfarande rött

--- BookRegister.Presentation/Program.cs
+++ BookRegister.Presentation/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using BookRegister.Application;
+using BookRegister.Infrastructure;
+using BookRegister.Presentation.Dialogs;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder();
+
+builder.Services.AddInfrastructure();
+builder.Services.AddApplication();
+
+builder.Services.AddTransient<BookDialog>();
+
+var app = builder.Build();
+
+var bookDialog = app.Services.GetRequiredService<BookDialog>();
+
+bookDialog.MainMenu();
