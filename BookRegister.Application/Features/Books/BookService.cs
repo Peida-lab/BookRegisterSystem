4ad7d9bb6be46df88b1b3d671724e850834fe14f
Ali Peida <din GitHub-email>
2026-09-12 00:54:49 +0200

    Add book service

--- BookRegister.Application/Features/Books/BookService.cs
+++ BookRegister.Application/Features/Books/BookService.cs
@@ -1,7 +1,55 @@
+using BookRegister.Application.Features.Books.Dtos.Requests;
+using BookRegister.Application.Features.Books.Dtos.Results;
 using BookRegister.Domain.Books;
 
 namespace BookRegister.Application.Features.Books;
+
 internal class BookService(IBookRepository bookRepository)
 {
-    bookRepo
+    public CreateBookResult CreateBook(CreateBookRequest request)
+    {
+        ArgumentNullException.ThrowIfNull(request);
+
+        Book book;
+        try
+        {
+            var bookId = Guid.NewGuid();
+
+            book = new Book
+            (
+                bookId, 
+                request.BookName, 
+                request.BookGenre
+            );
+        }
+
+
+        catch (Exception ex)
+        {
+            return new CreateBookResult(
+                false, 
+                null, 
+                ex.Message
+            );
+        }
+
+        bool saved = bookRepository.Create(book);
+
+        if(saved)
+        {
+            return new CreateBookResult
+            (
+                true, 
+                book, 
+                null
+            );
+        }
+
+        return new CreateBookResult
+        (
+            false,
+            null,
+            "Unable to save book"
+        );
+    }
 }
